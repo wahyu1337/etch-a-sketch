7dd04264cc18724313cd955c12f3ed23c5ecf18d
wahyu1337 <wahyuisnaeni2002@gmail.com>
2025-09-30 19:27:42 +0800

    ADD & UPDATED some code
    The code was build for the foundation. adding the
    first container from js to html.

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('PROJECT: Etch-a-Sketch')
+
+// Add title to the webpage (h1)
+const title = document.createElement('h1');
+title.textContent = 'Project Etch-a-Sketch'
+document.body.append(title);
+
+// adding a divs
+const container = document.createElement('div')
+container.classList.add('container');
+document.body.append(container);
+
